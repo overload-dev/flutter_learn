ae9c900c9a547a1b86bef7870bfe74af18b8c9dd
lee-kanghyo <khlee@pm-studios.com>
2021-09-24 17:19:05 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/overload/startlayout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.overload.startlayout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Row Widget
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* 1 */
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* 2 */
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeshinen Lake Camproground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const FavoriteWidget(),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 균등 배치
+        children:[
+          _buildButtonColumn(color, Icons.call, 'Call'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ]
+      )
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    Widget logoSection = Container(
+        padding: const EdgeInsets.all(32),
+        child: const LogoApp(),
+    );
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          leading: const IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null,
+          ),
+          title: const Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children:[
+            Image.asset(
+              'assets/images/lake.jpg',
+              width:600,
+              height:240,
+              fit: BoxFit.cover
+            ),
+            titleSection,
+            Divider(),
+            buttonSection,
+            Divider(),
+            textSection,
+            Divider(),
+            _title('Logo Action'),
+            logoSection,
+            Divider(),
+          ],
+        )
+      )
+    );
+  }
+
+  Text _title(String label) {
+    return Text(
+        label,
+        style: const TextStyle(
+            fontSize: 30.0,
+          color:Colors.blueAccent
+        )
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children:[
+        Icon(icon, color:color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color:color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+
+}
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget>{
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() { // 다시 렌더링
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? const Icon(Icons.star) : const Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Text('$_favoriteCount')
+        )
+      ],
+    );
+
+  }
+}
+
+class LogoApp extends StatefulWidget {
+  const LogoApp({Key? key}) : super(key: key);
+
+  @override
+  _LogoAppState createState() => _LogoAppState();
+
+}
+
+class _LogoAppState extends State<LogoApp> {
+  var _clickCount = 0;
+
+  void _onTapLogoCountUp(TapUpDetails detail){
+    setState((){
+      _clickCount++;
+    });
+  }
+
+  void _onTapLogoCountDown(){
+    debugPrint('long press');
+    setState((){
+      _clickCount--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTapUp:_onTapLogoCountUp,
+          onLongPress: _onTapLogoCountDown,
+          child: const FlutterLogo(),
+        ),
+        Text(
+          '$_clickCount',
+          style: const TextStyle(
+              fontSize: 15.0,
+              color:Colors.blueAccent
+          ),
+        ),
+      ],
+    );
+  }
+
+}
